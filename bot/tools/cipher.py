164d786ec788230b535d2c0a87dc8c05f49e00fb
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-12-12 12:17:01 +0100

    Lower hash to SHA256 because SHA512 hash was longer than the text to hash

--- bot/tools/cipher.py
+++ bot/tools/cipher.py
@@ -1,14 +1,13 @@
 import os
 
-from Crypto.Hash import SHA512
-
+from Crypto.Hash import SHA256
 
 # Reinitialized on restart
 RANDOM_SALT = os.urandom(64)
 
 
 def salted_digest(data):
-    hasher = SHA512.new()
+    hasher = SHA256.new()
     hasher.update(data)
     hasher.update(RANDOM_SALT)
     return hasher.digest()
