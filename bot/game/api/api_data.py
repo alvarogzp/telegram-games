bb85df8a9a8cecaf6f3db276347aa8f5f9b39d0c
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-12-12 11:37:42 +0100

    Remove newlines from exported url_data

--- bot/game/api/api_data.py
+++ bot/game/api/api_data.py
@@ -14,7 +14,7 @@ DATA_DIGEST_SEPARATOR = "-"
 def encode(data):
     dumped_data = json.dumps(data)
     encoded_dumped_data = dumped_data.encode(DATA_ENCODING)
-    encoded = codecs.encode(encoded_dumped_data, TRANSPORT_ENCODING)
+    encoded = _transport_encode(encoded_dumped_data)
     digest = salted_digest(encoded)
     return encoded.decode(DUMMY_ENCODING) + DATA_DIGEST_SEPARATOR + digest
 
@@ -25,9 +25,17 @@ def decode(data):
         encoded = encoded.encode(DUMMY_ENCODING)
         calculated_digest = salted_digest(encoded)
         if calculated_digest == digest:
-            encoded_dumped_data = codecs.decode(encoded, TRANSPORT_ENCODING)
+            encoded_dumped_data = _transport_decode(encoded)
             dumped_data = encoded_dumped_data.decode(DATA_ENCODING)
             return json.loads(dumped_data)
     except Exception:
         pass
     return {}
+
+
+def _transport_encode(data):
+    return codecs.encode(data, TRANSPORT_ENCODING).replace(b"\n", b"")
+
+
+def _transport_decode(data):
+    return codecs.decode(data, TRANSPORT_ENCODING)
