00ce3d2e5b75f7505f8e05b6bee426b6b50049b0
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-12-14 19:09:57 +0100

    Log score updates

--- bot/game/score.py
+++ bot/game/score.py
@@ -1,6 +1,7 @@
 import telegram
 
 from game.api import auth
+from tools.logger import Logger
 
 MAX_SCORE = 999999
 
@@ -8,10 +9,12 @@ MAX_SCORE = 999999
 class ScoreUpdater:
     def __init__(self, bot: telegram.Bot):
         self.bot = bot
+        self.logger = Logger(bot, tag="SCORE")
 
     def set_score(self, data, score):
         data = auth.decode(data)
         if data and score < MAX_SCORE:
+            self.logger.debug("Setting score of {} to {}".format(data, score))
             self._do_set_score(data["u"], data["i"], score)
 
     def _do_set_score(self, user_id, inline_message_id, score):
