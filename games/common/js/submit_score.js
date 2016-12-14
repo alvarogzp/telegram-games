ce8ad6c0121d5bb3d13ac799b4dcae8523376b53
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-12-14 11:43:54 +0100

    New submit_score.js version

--- games/common/js/submit_score.js
+++ games/common/js/submit_score.js
@@ -1,7 +1,16 @@
 function submit_score(score) {
+    auth_data = get_auth_data()
+    submit_data = auth_data + "&" + score
+    call_api("set_score", submit_data)
+}
+
+function get_auth_data() {
     hash = window.location.hash
-    auth_data = hash.substring(1, hash.indexOf("&"))
+    return hash.substring(1, hash.indexOf("&"))
+}
+
+function call_api(name, data) {
     request = new XMLHttpRequest()
-    request.open("POST", "https://cgb.ohbah.com:4343/cgbapi/set_score")
-    request.send(auth_data + "&" + score)
+    request.open("POST", "https://cgb.ohbah.com:4343/cgbapi/" + name)
+    request.send(data)
 }
