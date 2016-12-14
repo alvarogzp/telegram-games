954b81c35df2dfc34d1e57f12f770aa6c200bfdf
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-12-14 19:23:46 +0100

    Log game launches

--- bot/game/launch.py
+++ bot/game/launch.py
@@ -1,6 +1,8 @@
 import telegram
 
 from game.api import auth
+from tools.format import formatted_user
+from tools.logger import Logger
 
 
 def callback_query_game_launcher_handler(bot: telegram.Bot, update: telegram.Update):
@@ -11,6 +13,7 @@ def callback_query_game_launcher_handler(bot: telegram.Bot, update: telegram.Upd
         url_data = _build_url_data(callback_query)
         url = "https://rawgit.com/alvarogzp/telegram-games/develop/games/rock-paper-scissors/game.html#" + url_data
         bot.answerCallbackQuery(callback_query_id, url=url)
+        _log(bot, callback_query)
 
 
 def _build_url_data(callback_query: telegram.CallbackQuery):
@@ -19,3 +22,11 @@ def _build_url_data(callback_query: telegram.CallbackQuery):
         "i": callback_query.inline_message_id
     }
     return auth.encode(data)
+
+
+def _log(bot, callback_query):
+    user = formatted_user(callback_query.from_user)
+    game = callback_query.game_short_name
+    user_id = callback_query.from_user.id
+    inline_message_id = callback_query.inline_message_id
+    Logger(bot, tag="BOT").debug("Game '{}' launched by {} ({}, {})".format(game, user, user_id, inline_message_id))
